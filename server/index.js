a6b88e05eb6173a2e36ff39ca6f717c983675287
kreotiff-dev <48157839+kreotiff-dev@users.noreply.github.com>
2022-07-05 00:33:04 +0300

    /*swagger documentation*/

--- server/index.js
+++ server/index.js
@@ -7,19 +7,20 @@ const fileUpload = require('express-fileupload')
 const router = require('./routes/index')
 const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 const path = require('path')
-const swaggerJsDoc = require('swagger-jsdoc')
 const swaggerUI = require('swagger-ui-express')
 const {Brand} = require("./models/models");
-
+const YAML = require("yamljs");
+const swaggerJSDocs = YAML.load("./api.yaml");
 
 const PORT = process.env.PORT || 5000
-
 const app = express()
+
 app.use(cors())
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
 app.use('/api', router)
+app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(swaggerJSDocs));
 
 //Last Middleware
 app.use(errorHandler)
@@ -35,40 +36,5 @@ const start = async () => {
 }
 
 
-
-//Swagger https://swagger.io/specification/#infoObject
-const swaggerOptions = {
-    swaggerDefinition: {
-        info: {
-            title: "Documentation Test Store API",
-            description: 'API docs',
-            termsOfService: 'https://example.com/terms/',
-            contact: {
-                name: "Admin Api",
-                url: "http://localhost:5000",
-                email: "support@example.com"
-            },
-            license: {
-                name: "Apache",
-                url: "https://www.apache.org/licenses/LICENSE-2.0.html"
-            },
-            version: "1.0.0",
-            servers: {
-                    url: "http://localhost:5000",
-                    description: "Dev server"
-            }
-        }
-    },
-    apis: ["./routes/*.js"]
-}
-
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
-
-
 start()
-
-
-
-
 
--- server/routes/brandRouter.js
+++ server/routes/brandRouter.js
@@ -3,50 +3,31 @@ const router = new Router()
 const brandController = require('../controllers/brandController')
 const checkRole = require("../middleware/checkRoleMiddleware")
 
-
-
-/**
- * @swagger
- * components:
- *  schemas:
- *      Brand:
- *          type: object
- *          required:
- *              - name
- *          properties:
- *              id:
- *                  type: integer
- *                  format: int64
- *                  description: id brand
- *              name:
- *                  type: string
- *                  description: name brand
- *           example:
- *              id: 1102
- *              name: Vivo
- *              updatedAt: 2022-06-30T17:36:59.924Z
- *              createdAt: 2022-06-30T17:36:59.924Z
- *
- *
- */
-
 /**
  * @swagger
  * /api/brand:
  *  post:
- *      descriptions: Add new brand
+ *      summary: Add new brand
+ *      tags:
+ *          - API functions
+ *      parameters:
+ *      - name: "id"
+ *      required: true
+ *      schema:
+ *          type: string
+ *          example: string
  *      responses:
  *          '200':
- *              description: Success
- *              contents:
- *                  application/json:
- *                      schema:
- *                              $ref: '#/components/schemas/Brand'
+ *              description: OK
+ *              schema:
+ *              type: string
+ *              example: "some text"
  *  get:
- *      descriptions: Get All brands
+ *      summary: Get All brands
  *      responses:
  *          '200':
  *              description: Success
+ *
  *
  */
 router.post('/', checkRole('ADMIN'), brandController.create)
